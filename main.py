579b2b267e55a9420ed108a5d398c8edce8ab161
David Xue <dx8zp@virginia.edu>
2021-03-27 22:58:56 -0400

    added ml

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pandas as pd
 import streamlit as st
 from pandas_datareader import data
 from datetime import datetime, timedelta
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
+st.set_page_config(page_title='Stock Predictor', page_icon = 'icon.png')
 st.title("Stock Price Predictor")
-
-stock_name = st.text_input("Enter the stock name: \n")
-option = st.slider("How many days of data would you like to see?", 1, 60, 30)
-
+stock_name = st.text_input("Enter the stock name: \n", "AAPL")
+st.header('View Stock History')
+option = st.slider("How many days of data would you like to see?", 30, 180, 90)
 end = datetime.today().strftime('%Y-%m-%d')
 start = (datetime.today() - timedelta(option)).strftime('%Y-%m-%d')
 
@@ -25,11 +28,44 @@ data_load_state = st.text("Loading data...")
 df = load_data(stock=stock_name, start_date=start, end_date=end)
 df.sort_index(axis=0, inplace=True, ascending=False)
 
-st.subheader(f'{stock_name} stock prices for the past {option} days')
+st.subheader(f'{stock_name} Stock Prices for the Past {option} Days')
 st.dataframe(df)
 
 chart_data = df[['Close']]
-st.subheader("Close Prices")
+st.subheader("Prices at Close")
 st.line_chart(chart_data)
 
 data_load_state.text("Data loaded!")
+
+
+st.header('Stock Price Predictor')
+hist = []
+target = []
+length = option
+adj_close = df['Adj Close']
+for i in range(len(adj_close) - length):
+   x = adj_close[i:i+length]
+   y = adj_close[i+length]
+   hist.append(x)
+   target.append(y)
+
+hist = np.array(hist)
+target = np.array(target)
+target = target.reshape(-1,1)
+
+#train/test split
+X_train = hist[:1138]
+X_test = hist[1138:]
+y_train = target[:1138]
+y_test = target[1138:]
+
+sc = MinMaxScaler()
+#train set, fit_transform
+X_train_scaled = sc.fit_transform(X_train)
+y_train_scaled = sc.fit_transform(y_train)
+#test set, only transform
+X_test_scaled = sc.transform(X_test)
+y_test_scaled = sc.transform(y_test)
+
+X_train_scaled = X_train_scaled.reshape((len(X_train_scaled), length, 1))
+X_test_scaled = X_test_scaled.reshape((len(X_test_scaled), length, 1))
