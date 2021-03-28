bfe5fd726514ab38f607def8a80eb9bbd6e272de
David Xue <dx8zp@virginia.edu>
2021-03-28 03:47:07 -0400

    Fixed Major Calculation issues

--- main.py
+++ main.py
@@ -16,36 +16,34 @@ nltk.download(['punkt', 'wordnet'])
 
 def main():
     st.set_page_config(page_title='Stock Price Prediction Using News Headlines', page_icon = "icon.png")
-    st.sidebar.title("Stock Market DJIA Predictor News Headlines and Random Forest Classifier")
+    st.sidebar.title("Stock Market DJIA Predictor Using News Headlines and Random Forest Classifier")
+    st.title("Predicting Dow Jones Industrial Average Based on Headlines")
+    predStarted = False
+    if not predStarted:
+        st.subheader("Use the sidebar to start the prediction")
 
-    def load_data():
-        data = pd.read_csv(
-            'Combined_DJIA.csv')
-
+    def loadCleanData():
+        data = pd.read_csv('Combined_DJIA.csv')
         data['Top23'].fillna(data['Top23'].median, inplace=True)
         data['Top24'].fillna(data['Top24'].median, inplace=True)
         data['Top25'].fillna(data['Top25'].median, inplace=True)
-
         return data
 
-    def create_df(dataset):
+    def mergeToDF(dataset):
 
         dataset = dataset.drop(columns=['Date', 'Label'])
         dataset.replace("[^a-zA-Z]", " ", regex=True, inplace=True)
         for col in dataset.columns:
             dataset[col] = dataset[col].str.lower()
-
         headlines = []
         for row in range(0, len(dataset.index)):
             headlines.append(' '.join(str(x) for x in dataset.iloc[row, 0:25]))
-
-        dataset = load_data()
-
+        dataset = loadCleanData()
         df = pd.DataFrame(headlines, columns=['headlines'])
         df['label'] = dataset.Label
         df['date'] = dataset.Date
-
         return df
+
     def userInputCheck(data):
         data = data.lower()
         converted = pd.DataFrame([data], columns=['headlines'])
@@ -53,11 +51,10 @@ def main():
 
         return converted
 
-    def tokenize(text):
+    def tokenizeString(text):
         text = re.sub(r'[^\w\s]', '', text)
         tokens = word_tokenize(text)
         lemmatizer = WordNetLemmatizer()
-
         clean_tokens = []
         for token in tokens:
             clean_token = lemmatizer.lemmatize(token).lower().strip()
@@ -65,8 +62,7 @@ def main():
 
         return clean_tokens
 
-    def split(df):
-
+    def splitTrainTest(df):
         train = df[df['date'] < '20150101']
         test = df[df['date'] > '20141231']
         x_train = train.headlines
@@ -76,37 +72,32 @@ def main():
 
         return x_train, x_test, y_train, y_test
 
-    def plot_metrics(metrics_list):
+    def buildTrainReport():
+        st.subheader('Confusion Matrix')
+        predictions = model.predict(x_test)
+        matrix = confusion_matrix(y_test, predictions)
+        #st.write(type(matrix))
+        st.write(matrix)
+        st.subheader('Classification Report')
+        predictions = model.predict(x_test)
+        report = classification_report(y_test, predictions)
+        st.write(report)
+        st.subheader('Accuracy Score')
+        predictions = model.predict(x_test)
+        score = accuracy_score(y_test, predictions)
+        st.write("Accuracy Score: ", score.round(3))
 
-        if 'Confusion Matrix' in metrics_list:
-            st.subheader('Confusion Matrix')
-            predictions = model.predict(x_test)
-            matrix = confusion_matrix(y_test, predictions)
-            st.write("Confusion Matrix ", matrix)
-
-        if 'Classification_Report' in metrics_list:
-            st.subheader('Classification_Report')
-            predictions = model.predict(x_test)
-            report = classification_report(y_test, predictions)
-            st.write("Classification_Report ", report)
-
-        if 'Accuracy_Score' in metrics_list:
-            st.subheader('Accuracy_Score')
-            predictions = model.predict(x_test)
-            score = accuracy_score(y_test, predictions)
-            st.write("Accuracy_Score: ", score.round(2))
-
-    def Vectorize():
-        pipeline = Pipeline([
-            ('vect', CountVectorizer(tokenizer=tokenize, stop_words='english')),
+    def PipelineVectorize():
+        vectPipeline = Pipeline([
+            ('vect', CountVectorizer(tokenizer=tokenizeString, stop_words='english')),
             ('tfidf', TfidfTransformer())
         ])
-        return pipeline
+        return vectPipeline
 
-    df = load_data()
-    df = create_df(df)
-    x_train, x_test, y_train, y_test = split(df)
-    vector = Vectorize()
+    df = loadCleanData()
+    df = mergeToDF(df)
+    x_train, x_test, y_train, y_test = splitTrainTest(df)
+    vector = PipelineVectorize()
 
     if st.sidebar.checkbox("show train/test raw data", False):
         st.subheader("Top 25 Headline News from Reddit")
@@ -114,46 +105,54 @@ def main():
 
     classifier = "Random Forest Classifier"
 
-    
+
 
 
     if classifier == "Random Forest Classifier":
         st.sidebar.subheader("Random Forest Hyperparameters")
         n_estimators = st.sidebar.number_input(
             "n_estimators", 50, 300, step=50, key='n_estimators')
+        trainRes = st.sidebar.checkbox("show model training report", True)
+
         st.sidebar.subheader("Prediction")	
-        userInputLine = st.sidebar.text_area("Enter Your News Headline for Prediction:", "Despite 1,300 dead, majority of Palestinians still back Hamas rocket attacks")
-        metrics = ["Confusion Matrix", "Classification_Report", "Accuracy_Score"]
-        userInputLine = "b "+ userInputLine
-        userInputLine = userInputCheck(userInputLine)
+        userInputLine = st.sidebar.text_area("Enter Your News Headline for Prediction:", "Oklahoma Goodwill Employee Finds $42,000 Hidden in Donated Clothing –And Her Integrity Pays Off")
+
+        fixedInput = "b "+ userInputLine
+        h1 = fixedInput
+        fixedInput = ""
+        for i in range(25):
+            fixedInput += h1
+        fixedInput = userInputCheck(fixedInput)
         #st.write(f'{userInputLine.head(1)}')
 
 
         if st.sidebar.button("Predict", key="Predict"):
-
-            st.header("Results")
+            predStarted = True
+            st.spinner()
+            if trainRes:
+                st.header("Model Training Report using Kaggle Dataset")
+                st.write("Uncheck Sidebar Option to Hide Report")
             x_train = vector.fit_transform(x_train)
             x_test = vector.transform(x_test)
 
-            transInput = vector.transform(userInputLine)
+            transInput = vector.transform(fixedInput)
 
 
-            model = RandomForestClassifier(
-                n_estimators=n_estimators, criterion='entropy')
+            model = RandomForestClassifier(n_estimators=n_estimators, criterion='entropy', random_state=42)
             model.fit(x_train, y_train)
             predictions = model.predict(x_test)
 
             userPred = model.predict(transInput)
 
-
-            matrix = confusion_matrix(y_test, predictions)
-            score = accuracy_score(y_test, predictions)
-            report = classification_report(y_test, predictions)
-            # st.write("Accuracy_Score: ", score.round(2))
-            # st.write("Classification_Report ", report)
-            # st.write("Confusion Matrix ", matrix)
-            plot_metrics(metrics)
-            st.subheader("User Input Headline Prediction:")
+            if trainRes:
+                matrix = confusion_matrix(y_test, predictions)
+                score = accuracy_score(y_test, predictions)
+                report = classification_report(y_test, predictions)
+                buildTrainReport()
+                st.markdown('---')
+            st.header("Headline Input Prediction Result:")
+            st.write("Headline to be used for prediction:")
+            st.write(userInputLine)
             st.write(f'User Input Headline Prediction Result: {userPred[0]}')
             if userPred[0] == 1:
                 st.write("The Dow Jones Industrial Average will stay the same or more up today. Buy!")
